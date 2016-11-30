96e6ec0d17e5f232a00113ac1aaa4dfe0c70c5cb
Jens Ahrengot Boddum <ahrengot@gmail.com>
2016-11-30 18:16:09 +0100

    Update component to work with React v15.0.0+

--- src/react-svg-use.js
+++ src/react-svg-use.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import DOMProperty from 'react/lib/DOMProperty';
-
-// xlink:href is not a standardly-supported svg attribute, but you can tell
-// React that it is ok.
-DOMProperty.injection.injectDOMPropertyConfig({
-  isCustomAttribute: function (attributeName) {
-    return (attributeName === 'xlink:href');
-  }
-});
 
 export default props => {
+  let otherProps = Object.assign({}, props);
+  delete otherProps.id
+  delete otherProps.color
+
 	return (
-		<svg { ...props } >
-			{ React.createElement('use', { 'xlink:href': '#' + props.id, style: { fill: props.color } }) }
+		<svg { ...otherProps }>
+      <use xlinkHref={ `#${ props.id }` } style={{ fill: props.color }} />
 		</svg>
 	)
 }
